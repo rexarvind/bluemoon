78d9db744d9d417299b83a1db8900c5f6e6c945c
Arvind Kumar <thatisarvind@gmail.com>
2022-10-16 09:34:49 +0530

    update

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@ jQuery(function ($) {
 
     AOS.init();
 
-    window.scrollTo({ top:0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 
-    $('.navbar-toggler').on('click', function() {
-        jQuery('.headnev').css('background','#181e20e6');
+    $('.navbar-toggler').on('click', function () {
+        jQuery('.headnev').css('background', '#181e20e6');
     });
-    if ( jQuery(window).scrollTop() > 100 ) {
-        jQuery('.headnev').css('background','#181e20e6');
+    if (jQuery(window).scrollTop() > 100) {
+        jQuery('.headnev').css('background', '#181e20e6');
     }
     jQuery(window).bind('scroll', function () {
         if (jQuery(window).scrollTop() > 100) {
-            jQuery('.headnev').css('background','#181e20e6');
+            jQuery('.headnev').css('background', '#181e20e6');
         } else {
-            jQuery('.headnev').css('background','transparent');
+            jQuery('.headnev').css('background', 'transparent');
         }
     });
 
@@ -23,6 +23,32 @@ jQuery(function ($) {
         slidesToShow: 2,
         slidesToScroll: 1
     });
+
+    var type_here = document.getElementById("type_here");
+
+    var typewriter = new Typewriter(type_here, {
+        loop: true,
+        delay: 75
+    });
+
+    typewriter
+        // .pauseFor(2500)
+        .typeString("in crypto")
+        .pauseFor(300)
+        .deleteChars(6)
+        .typeString("bitcoin")
+        .pauseFor(100)
+        .deleteChars(7)
+        .typeString('paypal')
+        .pauseFor(100)
+        .deleteChars(6)
+        .typeString('this')
+        .pauseFor(100)
+        .deleteChars(4)
+        .typeString('that')
+        .pauseFor(100)
+        .start();
+
 
     const canvas = document.querySelector('#cwa_moon');
     const scene = new THREE.Scene();
@@ -40,22 +66,22 @@ jQuery(function ($) {
         height: canvas.clientHeight
     };
 
-    loader.load('https://stageversion.com/bluemoon/model/crate.glb', function ( gltf ) {
+    loader.load('https://stageversion.com/bluemoon/model/crate.glb', function (gltf) {
         moon = gltf.scene;
         moon.scale.set(1, 1, 1);
         moon.position.set(0, 0, -3.5);
         moon.rotation.set(0, 0, 0);
         scene.add(moon);
-    }, undefined, function ( error ) {
-        console.error( error );
+    }, undefined, function (error) {
+        console.error(error);
     });
 
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
-    scene.add( ambientLight );
+    scene.add(ambientLight);
 
     const directionalLight2 = new THREE.PointLight(0xf2f8ff, 10, 50);
     directionalLight2.position.set(8, 8, 15);
-    scene.add( directionalLight2 );
+    scene.add(directionalLight2);
 
     renderer.toneMapping = THREE.ReinhardToneMapping;
     renderer.toneMappingExposure = 1.3;
@@ -63,7 +89,7 @@ jQuery(function ($) {
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-    camera = new THREE.PerspectiveCamera(50, canvas.clientWidth / canvas.clientHeight, 1, 10000 );
+    camera = new THREE.PerspectiveCamera(50, canvas.clientWidth / canvas.clientHeight, 1, 10000);
     camera.position.set(0, 0, 0);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
     scene.add(camera);
@@ -75,7 +101,7 @@ jQuery(function ($) {
     }
     animate();
 
-    $(window).resize(function() {
+    $(window).resize(function () {
         // Update sizes
         sizes.width = canvas.clientWidth;
         sizes.height = canvas.clientHeight;
